5e9512b9e4c8742484bfa225ca5dc0439decb24b
Ranjan Jha <ranjanrocks9622@gmail.com>
2012-03-17 23:26:00 +0530

    faq page created

---
+++ application/views/faq/main_page.php
@@ -0,0 +1,41 @@
+<div class="row">
+      <div class="span11 offset1">
+        
+      	   			
+<h1> Frequently Asked Questions</h1>
+
+<h3>Q.My email address is already in use, but I never signed up.</h3>
+<p ><strong>Ans.</strong>We don't allow SignUp without any verification Id so there may be a case Any other an uautorised user register with ur Email Id and password so please Sign Up with Different Email Id .</p>
+
+<h3>Q.I never got my confirmation email!</h3>
+<p>Ans:The most common reason for this by far is the use of anti-spam measures. Especially those systems which require the sender of an e-mail to manually verify a message. The second most common reason for not getting e-mails is that the e-mail address entered does not exist, double check those e-mails! Finally, certain emailservices (such as msn and hotmail) seem to be better able to receive our messages to you if you have donotreply@gravatar.com added to your contacts already.</p>
+
+<h3>Q.I changed my information, but I still see my old one on other sites!</h3>
+<p>Ans.We cache your information for periods of time, which means that your new information will take effect after the old one expires from the cache. The reason we do this is to be able to serve your (and indeed everybody's) information much more quickly and reliably.</p>
+
+<h3>Q.How do I clear my cache?</h3>
+<p>Ans:This is a pretty concise guide on clearing your cache no matter what browser you use: http://kb.iu.edu/data/ahic.html.
+
+Basically your computer (in an attempt to make things faster for you) keeps copies of things (your avatar image, for example) on your computer, and the next time you request one of those things instead of getting it from the internet it gets that local saved copy. in this case you don't want the saved copy, you want the new one from the internet. Thats what clearing your cache does... it deletes all the local copies of things that your browser keeps around.
+</p>
+
+<h3>Q.Can I delete my uploaded images?</h3>
+<p>Ans:Yes, simply hover your mouse over them in the list at the bottom of your manage page, and click the red "X".
+</p>
+
+<h3>Q.How do I close my account?</h3>
+<p>Ans:Because of how our system works, we cannot delete accounts completely. U can only deactivate you account after that no one can se ur account.for again use ur account u just have to login on our site.
+</p>
+
+<h3>Q.How do I hide my profile?</h3>
+<p>Ans:While you cannot completely delete your profile, you can remove all of your profile information easily through the "Edit My Profile" page, and you can also Deactivate your profile from external access. Visit your profile page (click your name in the top navigation of the website), then click the "Deactivate My Profile"</p>
+
+<h3>Q.OK, but how do I contact support?</h3>
+<p>Ans:Send an e-mail to support@gidentity.com</p>
+
+<h3>Can i use one Email For Two Or more Id?</h3>
+<p>Ans: No one Email Id can be only Used For Profile.</p>
+
+</div>
+      </div>
+     
